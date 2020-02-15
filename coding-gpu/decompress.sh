05e270fa17f0c52bb5140a5351e45de3dae83654
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2020-02-15 17:41:27 -0600

    fix argument

--- coding-gpu/decompress.sh
+++ coding-gpu/decompress.sh
@@ -8,7 +8,7 @@ MODEL_PATH=$4
 
 
 if [ "$mode" = com ] ; then
-	python decompress_adaptive.py --file_name $BASE --output $OUTPUT --model_weight_path $MODEL_PATH
+	python decompress_adaptive.py --file_name $BASE --output $OUTPUT --model_weights_path $MODEL_PATH
 elif [ "$mode" = bs ] ; then
 	python decompress_bootstrap.py --filename $BASE --output $OUTPUT --model_weights_path $MODEL_PATH
 fi
